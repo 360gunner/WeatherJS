3ffdebd5aa1f2c1e47d7fb01c38bc965d299a8a4
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2019-09-29 21:57:31 +0200

    modified parameters 'unit'

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 const request = require("request")
 const private = require("./private")
-const url = private.url
+var url = private.url
 //the url is a link u get from the darksky.net api , make account they will give u yours
-request({url: url},(error,response) => {
-    console.log(response)
+url = url + "?units=si"
+request({ url: url, json: true},(error,response) => {
+    const degree = response.body.currently.temperature
+    
+    const proba = response.body.currently.precipProbability
+
+    console.log("It is currently "+degree+" degrees out.\nThere is a "+proba+"% chance of rain.")
 })
