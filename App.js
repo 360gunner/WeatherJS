c0e841984e9c4cb3bfa9995f1a0482196ac21407
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2019-10-03 18:21:05 +0200

    error handling

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 const request = require("request")
 const private = require("./private")
-var url = private.url
+var urldarksky = private.url
 //the url is a link u get from the darksky.net api , make account they will give u yours
-url = url + "?units=si"
-request({ url: url, json: true},(error,response) => {
-    const degree = response.body.currently.temperature
-    
-    const proba = response.body.currently.precipProbability
-
-    console.log("It is currently "+degree+" degrees out.\nThere is a "+proba+"% chance of rain.")
+urldarksky = urldarksky 
+var urlgeocode = "https://api.mapbox.com/geocoding/v5/mapbox.places/kolea%20tipaza.json?"+"limit=1&"+private.key
+//the key a private key for ur account u get it from the mapbox api website
+request({ url: urlgeocode, json: true }, (error, response) => {
+   if (error) {
+       console.log('Unable to connect to location services!')
+   } else if (response.body.features.length === 0) {
+       console.log('Unable to find location. Try another search.')
+   } else {
+       const latitude = response.body.features[0].center[0]
+       const longitude = response.body.features[0].center[1]
+       console.log(latitude, longitude)
+   }
 })
+
+
+
